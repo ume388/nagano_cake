4cddc803772f7755c03e744b3b2010a314600549
ume388 <turi388ouji@gmail.com>
2021-03-28 20:08:58 +0000

    [fix]商品投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '159e75fda78ace6312f580410575ac43db798c89a4b3c84f1604ddc15e1c35137fbbb7a10e8926fd6fbda69165b13d99bde86bb5539fe7304ec69a0cd8e12934'
