654ce7ed8143a0fea1ff4ed575d6dae5c1aee3d9
ume388 <turi388ouji@gmail.com>
2021-03-31 20:10:47 +0000

    [add]トップ、about画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :customers, controlles: {
+  devise_for :customers, controllers: {
     sessions: "customers/sessions",
     registrations: "customers/registrations",
   }
   
-  devise_for :admins, controlles: {
+  devise_for :admins, controllers: {
     sessions: "admins/sessions",
     registrations: "admins/registrations"
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'public/homes#top'
+  
+  get 'about' => 'public/homes#about'
+  
   namespace :public do
-    root to: 'homes#top'
   end
   
   namespace :admin do
