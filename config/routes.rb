cf8011ea8ed0ae12dd33c0fadd9453bc104dbf0e
ume388 <turi388ouji@gmail.com>
2021-03-31 20:55:06 +0000

    [add]ユーザ側商品一覧

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'public/homes#top'
-  
   get 'about' => 'public/homes#about'
+  get 'items' => 'public/items#index'
   
   namespace :public do
   end
