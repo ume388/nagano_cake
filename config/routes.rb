ea170c6dd76c37657905c32ce36f91d6553473b7
ume388 <turi388ouji@gmail.com>
2021-04-06 19:24:31 +0000

    [fix]コントローラ、削除部分の復活

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
   
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   
   scope module: 'public' do
     root to: 'homes#top'
