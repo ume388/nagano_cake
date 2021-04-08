7e5102ed082f6fa7a2550c6d290bb091680a3260
ume388 <turi388ouji@gmail.com>
2021-04-08 20:22:39 +0000

    [update]管理者ログイン後のルート指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    root_path
+    case resource
+      when Customer
+        root_path
+      when Admin
+        admin_root_path
+    end
   end
   
   protected
