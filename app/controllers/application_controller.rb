8ee6966221bc8256be830b2fb9077c707a9b4032
ume388 <turi388ouji@gmail.com>
2021-04-11 01:00:00 +0000

    [change]customerログイン後のルート

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
       when Customer
-        root_path
+        customers_my_page_path
       when Admin
         admin_root_path
     end
